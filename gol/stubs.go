6177c30d2807c9beea6c618bba76dfd18e7800d7
LeiLi0121 <128519250+LeiLi0121@users.noreply.github.com>
2023-11-21 17:35:00 +0000

    Update stubs.go

--- gol/stubs.go
+++ gol/stubs.go
@@ -1,14 +1,26 @@
 package gol
 
-var ExecuteTurns= "GolOp.ExecuteTurns"
+var ExecuteTurns = "GolOp.ExecuteTurns"
+var ExecuteTimer = "GolOp.Timer"
+var ExecuteKey = "GolOp.KeyOp"
+var KillProcess = "GolOp.Kill"
+var ResumeProcess = "GolOp.Resume"
 
 type Request struct {
 	World [][]uint8
-	P Params
+	P     Params
 }
 
 type Response struct {
-	NewWorld [][]uint8
-	Final FinalTurnComplete
+	NewWorld    [][]uint8
+	Final       FinalTurnComplete
+	CurrentTurn int
 }
 
+type KeyPress struct {
+	Key rune
+	P   Params
+}
+type ReportAlive struct {
+	Alive AliveCellsCount
+}
